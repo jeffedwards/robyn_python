eb11a440028148d3c370cc370d196c96e55adef1
Ayae Ide <ayaeide@fb.com>
2021-03-23 02:01:07 +0900

    lambdaRidge

--- python/fb_robyn_fun.py
+++ python/fb_robyn_fun.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import math
 import numpy as np
+from sklearn import preprocessing
 
 
 def rsq(true,predicted):
@@ -11,3 +12,23 @@ def rsq(true,predicted):
     sst = sum((true - sum(true)/len(true)) ** 2)
     return 1 - sse / sst
 
+
+def lambdaRidge(x, y, seq_len=100, lambda_min_ratio=0.0001):
+    def mysd(y):
+        mysd = math.sqrt(sum((y - sum(y) / len(y)) ** 2) / len(y))
+        return mysd
+
+    sx = x.apply(mysd)
+    sx = preprocessing.scale(x).T
+    sy = y.to_numpy()
+    sxy = sx * sy
+    sxy = sxy.T
+    # return sxy
+    lambda_max = max(abs(sxy.sum(axis=0)) / (
+                0.001 * x.shape[0]))  # 0.001 is the default smalles alpha value of glmnet for ridge (alpha = 0)
+    lambda_max_log = math.log(lambda_max)
+
+    log_step = (math.log(lambda_max) - math.log(lambda_max * lambda_min_ratio)) / (seq_len - 1)
+    log_seq = np.linspace(math.log(lambda_max), math.log(lambda_max * lambda_min_ratio), seq_len)
+    lambda_seq = np.exp(log_seq)
+    return lambda_seq
