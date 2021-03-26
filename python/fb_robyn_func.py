c7d216801a4d0914086d77e81767cfea7a604757
Ayae Ide <ayaeide@fb.com>
2021-03-26 16:41:46 +0900

    add parameter description

--- python/fb_robyn_func.py
+++ python/fb_robyn_func.py
@@ -9,6 +9,11 @@ from scipy import stats
 def rsq(true,predicted):
     """
     Define r-squared function
+    ----------
+    true: true value
+    predicted: predicted value
+    Returns r-squared
+    -------
     """
     sse = sum((predicted - true) ** 2)
     sst = sum((true - sum(true)/len(true)) ** 2)
@@ -18,21 +23,25 @@ def rsq(true,predicted):
 def lambdaRidge(x, y, seq_len=100, lambda_min_ratio=0.0001):
     """
     Define ridge lambda sequence function
+    ----------
+    x: matrix
+    y: vector
+    Returns lambda sequence
+    -------
+    array
     """
-    def mysd(y):
-        return math.sqrt(sum((y - sum(y) / len(y)) ** 2) / len(y))
+    def mysd(x):
+        return math.sqrt(sum((x - sum(x) / len(x)) ** 2) / len(x))
 
     sx = x.apply(mysd)
     sx = preprocessing.scale(x).T
     sy = y.to_numpy()
     sxy = sx * sy
     sxy = sxy.T
-    # return sxy
+
     lambda_max = max(abs(sxy.sum(axis=0)) / (
                 0.001 * x.shape[0]))  # 0.001 is the default smalles alpha value of glmnet for ridge (alpha = 0)
-    lambda_max_log = math.log(lambda_max)
 
-    log_step = (math.log(lambda_max) - math.log(lambda_max * lambda_min_ratio)) / (seq_len - 1)
     log_seq = np.linspace(math.log(lambda_max), math.log(lambda_max * lambda_min_ratio), seq_len)
     lambda_seq = np.exp(log_seq)
     return lambda_seq
@@ -55,7 +64,6 @@ def adstockGeometric(x, theta):
 
 def helperWeibull(x, y, vec_cum, n):
     """
-
     :param x:
     :param y:
     :param vec_cum:
@@ -97,7 +105,4 @@ def adstockWeibull(x, shape, scale):
     x_decayed = np.sum(x_decayed, axis=1).tolist()
 
     return x_decayed, thetaVecCum
-
-
-
 
