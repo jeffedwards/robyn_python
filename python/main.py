c2b73f481e4b2e0d9dee29aef4599e4df08c28c1
nuochen <nuochen1990@gmail.com>
2021-04-07 21:28:24 -0400

    added dictionary to main execution

--- python/main.py
+++ python/main.py
@@ -18,7 +18,28 @@ import scipy as sp
 from python import fb_robyn_func as f
 import python.setting as input
 
-
+# Create dictionary
+d = {
+    "df_Input": None,
+    "set_country": None,
+    "set_depVarName": None,
+    "activate_prophet": None,
+    "set_prophet": None,
+    "set_prophet_sign": None,
+    "activate_baseline": None,
+    "set_baseVarName": None,
+    "set_baseVarSign": None,
+    "set_mediaVarName": None,
+    "set_mediaSpendName": None,
+    "set_mediaVarSign": None,
+    "set_factorVarName": None,
+    "set_cores": None,
+    "adstock": None,
+    "set_iter": None,
+    "set_modTrainSize": None,
+    "set_hyperOptimAlgo": None,
+    "set_trial": None
+}
 
 # Uploading an Input file.
 # Specify the path where the input data file is located
